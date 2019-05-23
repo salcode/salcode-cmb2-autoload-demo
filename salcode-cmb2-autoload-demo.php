8de390bf289eef31f76443cd8922a683aa3a3a91
Sal Ferrarello <sal@ironcodestudio.com>
2019-05-23 06:09:06 -0400

    Initial commit
    Create an empty plugin and autoload CMB2

---
+++ salcode-cmb2-autoload-demo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Salcode CMB2 Autoload Demo
+ * Plugin URI: https://github.com/salcode/salcode-cmb2-autoload-demo
+ * Description:
+ * Version: 1.0.0
+ * Author: Sal Ferrarello
+ * Author URI: http://salferrarello.com/
+ * Text Domain: salcode-cmb2-autoload-demo
+ * Domain Path: /languages
+ *
+ * @package salcode-cmb2-autoload-demo
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
